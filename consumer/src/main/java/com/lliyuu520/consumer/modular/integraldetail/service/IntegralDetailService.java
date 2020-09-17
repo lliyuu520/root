df8c0c90ce448653dac9ab921418efd1e325c147
lliyuu520 <lliyuu520@gmail.com>
2020-09-17 14:10:37 +0800

    开始合版本

--- consumer/src/main/java/com/lliyuu520/consumer/modular/integraldetail/service/IntegralDetailService.java
+++
@@ -1,8 +0,0 @@
-package com.lliyuu520.consumer.modular.integraldetail.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.lliyuu520.api.modular.integraldetail.IntegralDetail;
-
-
-public interface IntegralDetailService extends IService<IntegralDetail> {
-}
--- provider/src/main/java/com/lliyuu520/provider/modular/integral/service/IntegralService.java
+++
@@ -1,9 +0,0 @@
-package com.lliyuu520.provider.modular.integral.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.lliyuu520.api.modular.integral.Integral;
-
-
-
-public interface IntegralService extends IService<Integral> {
-}
